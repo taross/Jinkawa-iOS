bf7c4557a041d6bee99a009e0e7f801c41839b29
Taro Sato <taross@Tarossbook.local>
2017-06-23 14:03:51 +0900

    Initial Commit

---
+++ Jinkawa-iOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Jinkawa-iOS
+//
+//  Created by Taro Sato on 2017/06/23.
+//  Copyright © 2017年 Taro Sato. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Jinkawa-iOSTests/Jinkawa_iOSTests.swift
@@ -0,0 +1,36 @@
+//
+//  Jinkawa_iOSTests.swift
+//  Jinkawa-iOSTests
+//
+//  Created by Taro Sato on 2017/06/23.
+//  Copyright © 2017年 Taro Sato. All rights reserved.
+//
+
+import XCTest
+@testable import Jinkawa_iOS
+
+class Jinkawa_iOSTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
