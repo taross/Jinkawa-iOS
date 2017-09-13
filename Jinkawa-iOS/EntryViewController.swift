53d011edb7dc2aea7b77ce6136951067c21c4b6c
Taro Sato <taross@Tarossbook.local>
2017-09-13 23:09:06 +0900

    [add] Eurekaの導入

---
+++ Jinkawa-iOS/EntryViewController.swift
@@ -0,0 +1,52 @@
+//
+//  EntryViewController.swift
+//  Jinkawa-iOS
+//
+//  Created by Taro Sato on 2017/09/13.
+//  Copyright © 2017年 Taro Sato. All rights reserved.
+//
+
+import UIKit
+import Eureka
+
+class EntryViewController: FormViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        form +++ Section("Section1")
+            <<< TextRow(){ row in
+                row.title = "Text Row"
+                row.placeholder = "Enter text here"
+            }
+            <<< PhoneRow(){
+                $0.title = "Phone Row"
+                $0.placeholder = "And numbers here"
+            }
+            +++ Section("Section2")
+            <<< DateRow(){
+                $0.title = "Date Row"
+                $0.value = Date(timeIntervalSinceReferenceDate: 0)
+        }
+        
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
